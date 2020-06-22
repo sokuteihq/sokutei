953d1869a02b690be97819fa94e945e92cf2a759
Vgeorgee <cracken1111@gmail.com>
2020-06-22 20:01:57 +0200

    changes for first PR

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 
 void iterate(int i){
     int a = 0;
-    SOKUTEI_DEFINE_COUNTER("ITER");
+    sokutei_create_integer_counter("ITER");
+    sokutei_create_float_counter("FLOAT");
     for(a = 0; a < i; a++){
-        SOKUTEI_INCREMENT_COUNTER("ITER");
+        sokutei_alter_integer_counter("FLOAT", 1);
+        sokutei_alter_float_counter("FLOAT", 1);
     }
 }
 
 int main() {
+    printf("%d\n", MAX_SIZE_OF_TYPES);
     iterate(10);
-    printf("%d\n", SOKUTEI_GET_COUNTER("ITER"));
+    printf("%d\n", sokutei_get_integer_counter("ITER"));
+    //printf("%f\n", SOKUTEI_GET_FLOAT_COUNTER("FLOAT"));
     return 0;
 }
--- sokutei.h
+++ sokutei.h
@@ -1,72 +1,188 @@
-#ifndef SOKUTEI_BENCHMARK
-#define SOKUTEI_BENCHMARK
+#ifndef SOKUTEI_BENCHMARK_H
+#define SOKUTEI_BENCHMARK_H
 
-#define MAX_COUNTERS 100
-#define COUNTER_LENGTH 32
-#define INT int
+#define SOKUTEI_ITERATIONS 10
+#define SOKUTEI_MAX_COUNTER_COUNT  100
+#define SOKUTEI_COUNTER_LENGTH 30
+#define SOKUTEI_INTEGER_COUNTER_TYPE int
+#define SOKUTEI_FLOAT_COUNTER_TYPE double
+#define SOKUTEI_INTERVAL_TIMER_COUNTER_TYPE int
 
-#ifdef SOKUTEI_NO_STDLIB
-#ifndef SOKUTEI_STDLIB
-#define SOKUTEI_STDLIB
+#define MAX_SIZE_OF_TYPES   sizeof(SOKUTEI_INTEGER_COUNTER_TYPE) > sizeof(SOKUTEI_FLOAT_COUNTER_TYPE) \
+                            ? sizeof(SOKUTEI_INTEGER_COUNTER_TYPE) > sizeof(SOKUTEI_INTERVAL_TIMER_COUNTER_TYPE) \
+                                ? sizeof(SOKUTEI_INTEGER_COUNTER_TYPE) \
+                                : sizeof(SOKUTEI_INTERVAL_TIMER_COUNTER_TYPE)\
+                            : sizeof(SOKUTEI_FLOAT_COUNTER_TYPE) > sizeof(SOKUTEI_INTERVAL_TIMER_COUNTER_TYPE) \
+                                ? sizeof(SOKUTEI_FLOAT_COUNTER_TYPE) \
+                                : sizeof(SOKUTEI_INTERVAL_TIMER_COUNTER_TYPE)
 
-int sokutei_strcmp(const char *a, const char *b){
+#define SOKUTEI_INTEGER_TYPE '1'
+#define SOKUTEI_FLOAT_TYPE '2'
+#define SOKUTEI_INTERVAL_TYPE '3'
+
+#define NOT_FOUND -1
+#define NOT_MATCHING_TYPE -2
+
+
+int sokutei_strcmp(const char *string_a, const char *string_b){
     int i = 0;
-    while(a[i] && b[i] && a[i] == b[i]) i++;
-    return a[i] - b[i];
+    while(string_a[i] == string_b[i] && string_a[i]) i++;
+    return string_a[i] - string_b[i];
 }
-#define STRCMP(a, b) sokutei_strcmp(a, b)
+#define STRCMP(string_a, string_b) sokutei_strcmp(string_a, string_b)
 
-char *sokutei_strcpy(char *a, const char *b){
+char *sokutei_strcpy(char *string_a, const char *string_b){
     int i;
-    for(i = 0; a[i] = b[i]; i++);
-    return a;
+    for(i = 0; (string_a[i] = string_b[i]) && (i < SOKUTEI_COUNTER_LENGTH); i++);
+    string_a[SOKUTEI_COUNTER_LENGTH] = '\0';
+    return string_a;
 }
-#define STRCPY(a, b) sokutei_strcpy(a, b)
+#define STRCPY(string_a, string_b) sokutei_strcpy(string_a, string_b)
+#define string_equals(string_a, string_b) (STRCMP(string_a, string_b) == 0)
 
-#endif //END OF SOKUTEI_STDLIB
-#else
-#include <string.h>
-#include <stdio.h>
-#define STRCMP(a, b) strcmp(a, b)
-#define STRCPY(a, b) strcpy(a, b)
 
-#endif //END OF SOKUTEI_NO_STDLIB
 
-char sokutei_int_counter_names[MAX_COUNTERS][COUNTER_LENGTH] = {'\0'};
-INT  sokutei_int_counters[MAX_COUNTERS] = {0};
-int  sokutei_int_number_of_counters = 0;
 
-void sokutei_add_counter(const char *ctr){
-    STRCPY(sokutei_int_counter_names[sokutei_int_number_of_counters++], ctr);
+
+
+char sokutei_counter_names[SOKUTEI_MAX_COUNTER_COUNT][SOKUTEI_COUNTER_LENGTH + 2] = {'\0'};
+char sokutei_counters[SOKUTEI_MAX_COUNTER_COUNT * MAX_SIZE_OF_TYPES] = {0};
+int  sokutei_number_of_counters = 0; //this buddy gets overwritten when positioned at 40. line
+
+
+
+char sokutei_get_type_of(int index){
+    return sokutei_counter_names[index][31];
 }
 
-int sokutei_int_increment_counter(const char *ctr, int by){
+
+int sokutei_get_index_of_counter(const char *counter_name){
     int i;
-    for(i = 0; i < sokutei_int_number_of_counters; i++){
-        if(STRCMP(sokutei_int_counter_names[i], ctr) == 0){
-            break;
+    printf("number of sokutei counters: %d\n", sokutei_number_of_counters);
+    for(i = 0; i < sokutei_number_of_counters; i++){
+        printf("|%s| |%s|", counter_name, sokutei_counter_names[i]);
+        if(string_equals(sokutei_counter_names[i], counter_name)){
+            return i;
         }
     }
-    if(i == sokutei_int_number_of_counters){
-        return -1;
-    }
-    return sokutei_int_counters[i] += by;
+    return NOT_FOUND;
 }
 
-int sokutei_int_get_counter(const char *ctr){
-    int i;
-    for(i = 0; i < sokutei_int_number_of_counters; i++){
-        if(STRCMP(sokutei_int_counter_names[i], ctr) == 0){
-            return sokutei_int_counters[i];
-        }
+
+void sokutei_add_counter(const char *counter_name, char type){
+
+    if(type != SOKUTEI_INTEGER_TYPE && type != SOKUTEI_FLOAT_TYPE && type != SOKUTEI_INTERVAL_TYPE){
+        return;
     }
-    return -1;
+    STRCPY(sokutei_counter_names[sokutei_number_of_counters], counter_name);
+    sokutei_counter_names[sokutei_number_of_counters][SOKUTEI_COUNTER_LENGTH + 1] = type;
+    printf("%s\n", sokutei_counter_names[sokutei_number_of_counters]);
+    sokutei_number_of_counters++;
 }
 
-#define SOKUTEI_DEFINE_COUNTER(x) sokutei_add_counter(x)
-#define SOKUTEI_INCREMENT_COUNTER(x) sokutei_int_increment_counter(x, 1)
-#define SOKUTEI_INCREMENT_COUNTER_BY(x, y) sokutei_int_increment_counter(x, y)
-#define SOKUTEI_GET_COUNTER(x) sokutei_int_get_counter(x)
+
+SOKUTEI_INTEGER_COUNTER_TYPE sokutei_integer_get_counter(const char *counter_name){
+    int index = sokutei_get_index_of_counter(counter_name);
+
+    if(index == NOT_FOUND){
+        return NOT_FOUND;
+    }
+    if(sokutei_get_type_of(index) != SOKUTEI_INTEGER_TYPE){
+        fprintf(stderr, "NOT MATCHING TYPE!\n");
+        return NOT_MATCHING_TYPE;
+    }
+    SOKUTEI_INTEGER_COUNTER_TYPE *counter = sokutei_counters + (index * MAX_SIZE_OF_TYPES);
+
+    if(&sokutei_number_of_counters == counter){
+        printf("BAJ VAN!");
+    }
+
+    return *counter;
+}
 
 
-#endif //END OF SOKUTEI_BENCHMARK
+SOKUTEI_INTEGER_COUNTER_TYPE //:)
+sokutei_integer_increment_counter(const char *counter_name, SOKUTEI_INTEGER_COUNTER_TYPE by){
+    int index = sokutei_get_index_of_counter(counter_name);
+
+    printf("\nindex = %d\n", index);
+    printf("MAX_SIZE_OF_TYPES = %llu\n", MAX_SIZE_OF_TYPES);
+    printf("index * MAX_SIZE_OF_TYPES = %llu\n", index * (int)MAX_SIZE_OF_TYPES);
+    if(index == NOT_FOUND){
+        return NOT_FOUND;
+    }
+    if(sokutei_get_type_of(index) != SOKUTEI_INTEGER_TYPE){
+        fprintf(stderr, "NOT MATCHING TYPE!\n");
+        return NOT_MATCHING_TYPE;
+    }
+    SOKUTEI_INTEGER_COUNTER_TYPE *counter = (void *)sokutei_counters + (index * MAX_SIZE_OF_TYPES);
+    printf("MERETE::||%d||\n", (void *)counter - (void *)sokutei_counters);
+    if(&sokutei_number_of_counters == counter){
+        printf("BAJ VAN!!");
+    }
+    return *counter += by;
+}
+
+
+
+SOKUTEI_FLOAT_COUNTER_TYPE sokutei_float_get_counter(const char *counter_name){
+    int index = sokutei_get_index_of_counter(counter_name);
+
+    if(index == NOT_FOUND){
+        return NOT_FOUND;
+    }
+    if(sokutei_get_type_of(index) != SOKUTEI_FLOAT_TYPE){
+        fprintf(stderr, "NOT MATCHING TYPE!\n");
+        return NOT_MATCHING_TYPE;
+    }
+
+    SOKUTEI_FLOAT_COUNTER_TYPE *counter = sokutei_counters + (index * MAX_SIZE_OF_TYPES);
+    if(&sokutei_number_of_counters == counter){
+        printf("BAJ VAN!!!");
+    }
+    return (SOKUTEI_FLOAT_COUNTER_TYPE) *counter;
+}
+
+
+SOKUTEI_FLOAT_COUNTER_TYPE sokutei_float_increment_counter(const char *counter_name, SOKUTEI_FLOAT_COUNTER_TYPE by){
+    int index = sokutei_get_index_of_counter(counter_name);
+
+    printf("%d\n", index);
+    if(index == NOT_FOUND){
+        return NOT_FOUND;
+    }
+
+    if(sokutei_get_type_of(index) != SOKUTEI_FLOAT_TYPE){
+        fprintf(stderr, "NOT MATCHING TYPE!\n");
+        return NOT_MATCHING_TYPE;
+    }
+
+    SOKUTEI_FLOAT_COUNTER_TYPE *counter = sokutei_counters + (index * MAX_SIZE_OF_TYPES);
+    printf("MERETE::||%d||\n", (void *)counter - (void *)sokutei_counters);
+
+    if(&sokutei_number_of_counters == counter){
+        printf("BAJ VAN!!!!");
+    }
+    return *counter += by;
+}
+
+
+/// Counter creations
+#define SOKUTEI_DEFINE_COUNTER(counter_name, type) sokutei_add_counter(counter_name, type)
+#define sokutei_create_integer_counter(counter_name) SOKUTEI_DEFINE_COUNTER(counter_name, SOKUTEI_INTEGER_TYPE)
+#define sokutei_create_float_counter(counter_name) SOKUTEI_DEFINE_COUNTER(counter_name, SOKUTEI_FLOAT_TYPE)
+#define sokutei_create_interval_counter(counter_name) SOKUTEI_DEFINE_COUNTER(counter_name, SOKUTEI_INTERVAL_TYPE)
+
+
+/// Integer counter operations
+//#define SOKUTEI_INCREMENT_INTEGER_COUNTER(x) sokutei_integer_increment_counter(x, 1)
+#define sokutei_alter_integer_counter(x, y) sokutei_integer_increment_counter(x, y)
+#define sokutei_get_integer_counter(x) sokutei_integer_get_counter(x)
+
+//#define sokutei_alter_float_counter(x) sokutei_float_increment_counter(x, 1.0)
+#define sokutei_alter_float_counter(x, y) sokutei_float_increment_counter(x, y)
+#define sokutei_get_float_counter(x) sokutei_float_get_counter(x)
+
+
+
+#endif //END OF SOKUTEI_BENCHMARK_H
