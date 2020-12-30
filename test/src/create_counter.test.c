2e7e589936542ebffeca612adf085812a903a686
VGeorgee <cracken1111@gmail.com>
2020-12-30 13:33:03 +0100

    fix: missing semicolon

--- test/src/create_counter.test.c
+++ test/src/create_counter.test.c
@@ -1,7 +1,8 @@
 #include "../../greatest.h"
 #include "../../sokutei.h"
 
-TEST sokutei_add_counter_should_add_a_new_non_existing_counter_and_return_without_any_error(void){
+TEST sokutei_add_counter_should_add_a_new_non_existing_counter_and_return_without_any_error(void)
+{
     //given
     const char *counter_name = "a new counter";
     const int expected = 0;
@@ -16,7 +17,8 @@ TEST sokutei_add_counter_should_add_a_new_non_existing_counter_and_return_withou
     PASS();
 }
 
-TEST sokutei_add_counter_should_not_add_an_existing_counter_and_should_return_with_an_already_exists_error(void){
+TEST sokutei_add_counter_should_not_add_an_existing_counter_and_should_return_with_an_already_exists_error(void)
+{
     //given
     const char *counter_name = "a new counter 2";
     sokutei_add_counter(counter_name, SOKUTEI_INTEGER_TYPE);
@@ -32,11 +34,11 @@ TEST sokutei_add_counter_should_not_add_an_existing_counter_and_should_return_wi
     PASS();
 }
 
-
-TEST sokutei_add_counter_should_not_add_an_invalid_type_counter_and_should_return_with_a_not_matching_type_error(void){
+TEST sokutei_add_counter_should_not_add_an_invalid_type_counter_and_should_return_with_a_not_matching_type_error(void)
+{
     //given
     const char *counter_name = "name of the invalid counter";
-    const int invalid_counter_type = 199
+    const int invalid_counter_type = 199;
     const int expected = SOKUTEI_NOT_MATCHING_TYPE;
     const int expected_number_of_counters = sokutei_number_of_counters;
 
@@ -49,15 +51,17 @@ TEST sokutei_add_counter_should_not_add_an_invalid_type_counter_and_should_retur
     PASS();
 }
 
-SUITE(create_counter_tests) {
-        RUN_TEST(sokutei_add_counter_should_add_a_new_non_existing_counter_and_return_without_any_error);
-        RUN_TEST(sokutei_add_counter_should_not_add_an_existing_counter_and_should_return_with_an_already_exists_error);
-        RUN_TEST(sokutei_add_counter_should_not_add_an_invalid_type_counter_and_should_return_with_a_not_matching_type_error);
+SUITE(create_counter_tests)
+{
+    RUN_TEST(sokutei_add_counter_should_add_a_new_non_existing_counter_and_return_without_any_error);
+    RUN_TEST(sokutei_add_counter_should_not_add_an_existing_counter_and_should_return_with_an_already_exists_error);
+    RUN_TEST(sokutei_add_counter_should_not_add_an_invalid_type_counter_and_should_return_with_a_not_matching_type_error);
 }
 
 GREATEST_MAIN_DEFS();
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     GREATEST_MAIN_BEGIN();
 
     RUN_SUITE(create_counter_tests);
