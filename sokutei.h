2dc56e0a9284a76668319efa090ba6b3fe445f6c
Vgeorgee <cracken1111@gmail.com>
2021-02-07 19:14:57 +0100

    feat: add get_timer_counter function

--- sokutei.h
+++ sokutei.h
@@ -341,6 +341,22 @@ SOKUTEI_FLOAT_COUNTER_TYPE sokutei_float_increment_counter(const char *counter_n
 
 ///--- Counter Getter and Setter functions
 
+/// Timer counter functions
+
+SOKUTEI_TIMER_TYPE *sokutei_get_timer_counter(const char *counter_name){
+    const int index = sokutei_get_index_of_counter(counter_name);
+    if(index == SOKUTEI_NOT_FOUND){
+        return SOKUTEI_NOT_FOUND;
+    }
+    if(sokutei_get_type_of(index) != SOKUTEI_INTERVAL_TYPE){
+        //TODO call error
+        return SOKUTEI_NOT_MATCHING_TYPE;
+    }
+    return sokutei_counter_at_index(SOKUTEI_TIMER_TYPE, index);
+}
+
+
+///--- Timer counter functions
 
 /// Reporting functions
 
