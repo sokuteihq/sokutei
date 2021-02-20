de8a8aa3bf5110faa2d42c88d64aafef51acce74
Vgeorgee <cracken1111@gmail.com>
2021-02-20 15:35:16 +0100

    feat: add default print function to Windows header

--- platforms/sokutei-windows.h
+++ platforms/sokutei-windows.h
@@ -3,6 +3,12 @@
 #ifndef SOKUTEI_SOKUTEI_PLATFORM_H
 #define SOKUTEI_SOKUTEI_PLATFORM_H
 
+#   include <stdio.h>
+
+#   ifndef sokutei_print_string
+#       define sokutei_print_string(string) printf("%s", string);
+#   endif
+
 #   include <time.h>
 
     typedef struct counter_type {
