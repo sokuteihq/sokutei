79789246c6862e4d8e47629092da67449b87389a
Vgeorgee <cracken1111@gmail.com>
2021-02-27 23:52:17 +0100

    fix: fix stop function name

--- platforms/sokutei-cuda.h
+++ platforms/sokutei-cuda.h
@@ -1,25 +1,22 @@
 #if (defined(__NVCC__))
 
-#ifndef SOKUTEI_SOKUTEI_PLATFORM_H
-#define SOKUTEI_SOKUTEI_PLATFORM_H
-
     typedef struct couter_type{
         cudaEvent_t start;
         cudaEvent_t stop;
         unsigned int elapsed;
-    } SOKUTEI_TIMER_TYPE;
+    } CUDA_TIMER_TYPE;
 
 
     SOKUTEI_TIMER_TYPE *sokutei_get_timer_counter(const char *);
 
-    void sokutei_timer_start_function(char *counter_name){
+    void cuda_timer_start_function(char *counter_name){
         SOKUTEI_TIMER_TYPE *counter = sokutei_get_timer_counter(counter_name);
         cudaEventCreate(&(counter->start));
         cudaEventCreate(&(counter->stop));
         cudaEventRecord(counter->start);
     }
 
-    void sokutei_timer_start_function(char *counter_name){
+    void cuda_timer_stop_function(char *counter_name){
         SOKUTEI_TIMER_TYPE *counter = sokutei_get_timer_counter(counter_name);
         cudaEventRecord(counter->stop);
         cudaEventSynchronize(counter->stop);
@@ -27,18 +24,19 @@
     }
 
 #   ifndef SOKUTEI_TIMER_COUNTER_TYPE
-#       define SOKUTEI_TIMER_COUNTER_TYPE SOKUTEI_TIMER_TYPE
+#       define SOKUTEI_TIMER_COUNTER_TYPE CUDA_TIMER_TYPE
 #   endif
 
 #   ifndef SOKUTEI_TIMER_START
-#       define SOKUTEI_TIMER_START sokutei_timer_start_function
+#       define SOKUTEI_TIMER_START cuda_timer_start_function
 #   endif
 
 #   ifndef SOKUTEI_TIMER_STOP
-#       define SOKUTEI_TIMER_STOP sokutei_timer_stop_function
+#       define SOKUTEI_TIMER_STOP cuda_timer_stop_function
 #   endif
 
 #   ifndef SOKUTEI_CUSTOM_TIMER
 #   endif //SOKUTEI_CUSTOM_TIMER
+
 #endif //CONDITIONAL
 #endif //SOKUTEI_SOKUTEI_PLATFORM_H
